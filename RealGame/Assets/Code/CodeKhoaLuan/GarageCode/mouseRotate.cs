d92b88d8df086c62a2e7856c3054a8d1ba92bb0a
Thinhnnn <thinhtm99@gmail.com>
2021-06-29 08:35:20 +0700

    update garage UI

---
+++ RealGame/Assets/Code/CodeKhoaLuan/GarageCode/mouseRotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseRotate : MonoBehaviour
+{
+    [SerializeField] GameObject vertical;
+
+    [SerializeField] float ySpeed, xSpeed, maxXAngle;
+    
+    float ySpin, xSpin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ySpin = Input.GetAxis("Mouse X");
+        xSpin = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0f, ySpin * ySpeed * Time.deltaTime, 0f);
+        xSpin = Mathf.Clamp(xSpin, -maxXAngle, maxXAngle);
+        vertical.transform.Rotate(-xSpin * xSpeed * Time.deltaTime, 0f, 0f);
+    }
+}
---
+++ RealGame/Assets/Code/CodeKhoaLuan/GarageCode/playerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    float x, z;
+
+    [SerializeField] GameObject vision;
+
+    [SerializeField] float speed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        transform.position += (vision.transform.forward * z + vision.transform.right * x) * speed * Time.deltaTime;
+    }
+}
